0e358a953c1830bc9e2fcb61620c6f13fd23ca0e
Aidan Melvin <aidmelvin.314@gmail.com>
2022-05-09 08:27:42 -0500

    logging login attempts to /var/log/logins.txt

--- webserver/backend.ts
+++ webserver/backend.ts
@@ -1,6 +1,7 @@
 
 import express, { Request, Response } from "express";
 import cors from "cors";
+import fs from 'fs';
 
 const app = express();
 
@@ -25,8 +26,18 @@ app.get("/static/*", (req, res) => {
 });
 
 app.post('/login', (req, res) => {
-    console.log("received  post request");
-    res.redirect("/incorrect");
+    console.log("received post request");
+    let body = "";
+    req.on('data', (d) => {
+        body += d;
+    });
+    req.on('end', () => {
+        console.log('request body: ', body);
+        fs.writeFile("/var/log/logins.txt", "utf-8", () => {
+            res.redirect("/incorrect");
+        });
+    });
+    // res.redirect("/incorrect");
 });
 
 app.get("/incorrect", (req, res) => {
